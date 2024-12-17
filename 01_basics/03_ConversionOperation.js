16371bc79bab23ef828b721db81d0d9186d1fe22
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-12-17 10:20:46 +0000

    conversion of datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);//NaN
+
+//"33"=>33
+// "33abc"=>NaN
+// true=> 1; false=>0
+
+let isLoggedIn ="rahul"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+//1 =>terue ; 0 =>false
+// "" =>false
+// "rahul" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
