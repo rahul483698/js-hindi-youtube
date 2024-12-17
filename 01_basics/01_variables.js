030c6efd7d34852ee790e640c4fff39684067a53
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-12-17 10:01:40 +0000

    variable and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=14356
+let accountEmail="rahul123@8709.com"
+var accountPassword="12323"
+accountCity="jaipur"
+let accountState;
+
+/* prefer not to use var 
+because of issues in block scope and functional scope
+*/
+
+//accountId=2 //not allowed
+accountEmail="hsjdd@.com"
+accountPassword="212121"
+accountCity="patna"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we use using nodejs , not browser
+ 
+/* dataType */
+
+let name ="rahul" //string
+let age = 19      //int
+let isLoggedIn =false//boolean
+let state =null
+
+//bigint
+//boolean=>true/false
+//null=>standalone value
+//undefined=>abhi value assign nahi hui hai
+// symbol =>unique
+
+//object
+
+console.log(typeof "rahul");
+console.log(typeof null);//object
+
+
