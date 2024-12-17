fec90b2a1a4de6f9a95adfa442d5787363759a98
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-12-17 10:48:01 +0000

    operation

---
+++ 01_basics/04_0peration.js
@@ -0,0 +1,20 @@
+let value =3
+let negValue = -value
+//console.log(negValue); //=> -3
+
+let str1 = "rahul"
+let str2 = " kumar"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(+true); //true
+// console.log(+""); // false
+
+let gameCounter = 100
+++gameCounter; //prefix 
+console.log(gameCounter);
