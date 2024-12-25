a5899395db982591b4e4b801271f4d295f213546
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-12-25 02:30:03 +0000

    objectFirst

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,11 +1,15 @@
 //singleton
 //object.create
 
+const mySem = Symbol("key1");
+
+
 //object literals
 const jsUser = {
     name:"rahul",
     "fullName":"rahul kumar",//isko access karne ke liye=>jsUser["fullName"]
     age: 19,
+    [mySem]:"mykey1",
     location: "siwan",
     email: "rahul@google.com",
     isLoggedIn: false,
@@ -13,6 +17,23 @@ const jsUser = {
 
 }
 
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["fullName"]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["fullName"]);
+// console.log(jsUser[mySem]);
+
+jsUser.email="rahul@chatgpt.com";
+//Object.freeze(jsUser); //bad me change nahi hoga freeze karne se
+jsUser.email="rahul@microsoft.com";
+//console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("hello jsuser");
+}
+jsUser.greetingTwo =function(){
+    console.log(`hello js user ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
