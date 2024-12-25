2910ae5ec7b77f5941e32b5eb05093d8e3bd4f44
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2024-12-25 13:12:12 +0000

    object2

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -16,11 +16,36 @@ const regularUser ={
     }
 }
 
-console.log(regularUser.fullName.userFullName.firstName);
+//console.log(regularUser.fullName.userFullName.firstName);
 
 const obj1={1:"one",2:"two"};
 const obj2={3:"three",4:"four"};
 //const obj3 = Object.assign({},obj1,obj2);
 const obj3 = {...obj1,...obj2};
 
-console.log(obj3);
+
+//console.log(obj3);
+
+const users = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+users[1].email="r@123"
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));//checked property in object 
+
+
+
