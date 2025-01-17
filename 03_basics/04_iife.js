cab4029c0bc603e4c963125e2f07061e2d6dd0e2
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2025-01-17 13:31:26 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+//Immadiately Invoked function Expression(iife),global polution se bachne ke liye use kiya jata hai
+
+(function chai(){
+    //named IIFE
+    console.log(`data base connected`)
+})();//semicolom very very importent agar do IIFE likhna ho to nahi to second IIFE error dega
+
+((name)=>{
+    //Unkown IIfe
+    console.log(`data base 2 connected ${name}`)
+})("rahul");
+
