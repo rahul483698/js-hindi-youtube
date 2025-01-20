8bf61d5be85100cb88f2af9e13020ec3752ce04b
rahul483698 <137701369+rahul483698@users.noreply.github.com>
2025-01-20 06:05:05 +0000

    loops

---
+++ 05_iteration/one.js
@@ -0,0 +1,44 @@
+//for
+
+for(let index=0;index<=10;index++){
+    const element = index;
+    if(index==5){
+        //console.log("5 is a best number")
+    }
+    //console.log(element)
+}
+
+for(let i=0; i<=10; i++){
+    //console.log(`outer loop ${i}`)
+    for(let j=0; j<=10; j++){
+        //console.log(`inner loop ${j}`)
+
+    }
+}
+
+let myarray = ["rahul","rohit","ankit","prince","muna"]
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    //console.log(element);
+    
+}
+
+// break and continue
+
+for (let index = 0; index <= 20; index++) {
+    if(index==5){
+        console.log("detect 5")
+        break
+    }
+    console.log(`value of index ${index}`)
+    
+}
+for (let index = 0; index <= 20; index++) {
+    if(index==5){
+        console.log("detect 5")
+        continue
+    }
+    console.log(`value of index ${index}`)
+    
+}
+
